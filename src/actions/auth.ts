22b01dfa1fedc73ea19dbffb5dd63a2478d7d716
yoohyunk <yoohyunk20@gmail.com>
2024-11-08 23:06:34 -0700

    create auth page with signin and signup

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -15,11 +15,44 @@ export const signIn = async (email: string, password: string) => {
       throw new Error(data.error);
     }
 
-    localStorage.setItem("token", data.jwt);
+    localStorage.setItem("auth-token", data.jwt);
 
     return data.jwt;
   } catch (error) {
     console.log("Error logging in", error);
+    throw error;
+  }
+};
+
+export const signUp = async (
+  email: string,
+  password: string,
+  confirmPassword: string
+) => {
+  if (password !== confirmPassword) {
+    throw new Error("Passwords do not match");
+  }
+  try {
+    const response = await fetch(
+      "https://flask-server-6y1b.onrender.com/users/signup",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error);
+    }
+
+    localStorage.setItem("auth-token", data.jwt);
+
+    return data.jwt;
+  } catch (error) {
+    console.log("Error signing up", error);
     throw error;
   }
 };
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -9,11 +9,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { signIn } from "@/actions/auth";
+import { signIn, signUp } from "@/actions/auth";
 import React, { useState } from "react";
 
 export default function page() {
   const [error, setError] = useState<string | null>(null);
+  const [currentForm, setCurrentForm] = useState<"login" | "signup">("login");
+  const toggleForm = () => {
+    setCurrentForm(currentForm === "login" ? "signup" : "login");
+    setError(null);
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen">
@@ -22,24 +27,48 @@ export default function page() {
           e.preventDefault();
           const target = e.target as HTMLFormElement;
           try {
-            await signIn(
-              (target.elements.namedItem("email") as HTMLInputElement).value,
-              (target.elements.namedItem("password") as HTMLInputElement).value
-            );
-          } catch (error) {
-            if (error.message === "email or password incorrect") {
-              setError("Email or password is incorrect");
+            if (currentForm === "signup") {
+              await signUp(
+                (target.elements.namedItem("email") as HTMLInputElement).value,
+                (target.elements.namedItem("password") as HTMLInputElement)
+                  .value,
+                (
+                  target.elements.namedItem(
+                    "confirmPassword"
+                  ) as HTMLInputElement
+                ).value
+              );
             } else {
-              setError("An error occurred. Please try again later.");
+              await signIn(
+                (target.elements.namedItem("email") as HTMLInputElement).value,
+                (target.elements.namedItem("password") as HTMLInputElement)
+                  .value
+              );
             }
-            console.error("Error logging in", error);
+          } catch (error) {
+            if (error instanceof Error) {
+              if (error.message === "email or password incorrect") {
+                setError("Email or password is incorrect");
+              } else if (error.message === "Passwords do not match") {
+                setError("Passwords do not match");
+              } else if (error.message === "email already exists") {
+                setError("Email already exists");
+              } else {
+                setError("An error occurred. Please try again later.");
+              }
+            } else {
+              setError("An unknown error occurred.");
+            }
+            console.log("Error logging in", error);
           }
         }}
         className="flex items-center justify-center w-2/5"
       >
         <Card className="flex-col items-center size-2/4 gap-y-8">
           <CardHeader>
-            <CardTitle className="text-xl text-center">Sign in</CardTitle>
+            <CardTitle className="text-xl text-center">
+              {currentForm === "login" ? "Sign in" : "Sign up"}
+            </CardTitle>
           </CardHeader>
 
           <CardContent className="flex flex-col gap-4">
@@ -49,6 +78,15 @@ export default function page() {
             <div>
               <Input type="password" id="password" placeholder="Password" />
             </div>
+            {currentForm === "signup" && (
+              <div>
+                <Input
+                  type="password"
+                  id="confirmPassword"
+                  placeholder="Confirm Password"
+                />
+              </div>
+            )}
           </CardContent>
           <CardFooter className="flex flex-col gap-2">
             <Button
@@ -56,16 +94,28 @@ export default function page() {
               variant="outline"
               className="bg-black text-slate-50"
             >
-              Sign In
+              {currentForm === "login" ? "Sign in" : "Sign up"}
             </Button>
             {error && (
               <CardDescription className="text-xs text-red-600">
                 {error}
               </CardDescription>
             )}
-            <CardDescription className="text-xs">
-              Don't have account? Sign up
-            </CardDescription>
+            {currentForm === "login" ? (
+              <CardDescription className="text-xs">
+                Don't have account?{" "}
+                <span onClick={toggleForm} className="text-blue-500 underline">
+                  Sign up
+                </span>
+              </CardDescription>
+            ) : (
+              <CardDescription className="text-xs">
+                Already have account?{" "}
+                <span onClick={toggleForm} className="text-blue-500 underline">
+                  Sign in
+                </span>
+              </CardDescription>
+            )}
           </CardFooter>
         </Card>
       </form>
