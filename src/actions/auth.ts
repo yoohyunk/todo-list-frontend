d07df7285d0d482eb4f487dfae7ced5b36d4221f
yoohyunk <yoohyunk20@gmail.com>
2024-11-08 19:35:33 -0700

    create log in flow

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,3 +1,25 @@
-export const signIn = (email: string, password: string) => {
-  alert("your email is: " + email + " and your password is: " + password);
+export const signIn = async (email: string, password: string) => {
+  try {
+    const response = await fetch(
+      "https://flask-server-6y1b.onrender.com/users/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error);
+    }
+
+    localStorage.setItem("token", data.jwt);
+
+    return data.jwt;
+  } catch (error) {
+    console.log("Error logging in", error);
+    throw error;
+  }
 };
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -10,39 +10,65 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { signIn } from "@/actions/auth";
+import React, { useState } from "react";
 
 export default function page() {
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        const target = e.target as HTMLFormElement;
-        signIn(
-          (target.elements.namedItem("email") as HTMLInputElement).value,
-          (target.elements.namedItem("password") as HTMLInputElement).value
-        );
-      }}
-      className="flex items-center justify-center mt-14"
-    >
-      <Card className="flex-col items-center size-2/4 gap-y-8">
-        <CardHeader>
-          <CardTitle className="text-xl text-center">Sign in</CardTitle>
-        </CardHeader>
+  const [error, setError] = useState<string | null>(null);
 
-        <CardContent className="flex-col gap-5">
-          <div>
-            <Input type="email" id="email" placeholder="Email" />
-          </div>
-          <div>
-            <Input type="password" id="password" placeholder="Password" />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button type="submit" variant="outline">
-            Button
-          </Button>
-        </CardFooter>
-      </Card>
-    </form>
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const target = e.target as HTMLFormElement;
+          try {
+            await signIn(
+              (target.elements.namedItem("email") as HTMLInputElement).value,
+              (target.elements.namedItem("password") as HTMLInputElement).value
+            );
+          } catch (error) {
+            if (error.message === "email or password incorrect") {
+              setError("Email or password is incorrect");
+            } else {
+              setError("An error occurred. Please try again later.");
+            }
+            console.error("Error logging in", error);
+          }
+        }}
+        className="flex items-center justify-center w-2/5"
+      >
+        <Card className="flex-col items-center size-2/4 gap-y-8">
+          <CardHeader>
+            <CardTitle className="text-xl text-center">Sign in</CardTitle>
+          </CardHeader>
+
+          <CardContent className="flex flex-col gap-4">
+            <div>
+              <Input type="email" id="email" placeholder="Email" className="" />
+            </div>
+            <div>
+              <Input type="password" id="password" placeholder="Password" />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col gap-2">
+            <Button
+              type="submit"
+              variant="outline"
+              className="bg-black text-slate-50"
+            >
+              Sign In
+            </Button>
+            {error && (
+              <CardDescription className="text-xs text-red-600">
+                {error}
+              </CardDescription>
+            )}
+            <CardDescription className="text-xs">
+              Don't have account? Sign up
+            </CardDescription>
+          </CardFooter>
+        </Card>
+      </form>
+    </div>
   );
 }
