15e8fac71491708f59999842a79cc4a0bb7fc6bc
yoohyunk <yoohyunk20@gmail.com>
2024-11-14 00:31:52 -0700

    fix status checkbox error

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -32,7 +32,7 @@ export const TodoComponent = ({
             <div className="flex items-center gap-4">
               <TooltipProvider>
                 <Tooltip>
-                  <TooltipTrigger>
+                  <TooltipTrigger asChild>
                     <StatusCheckBox listId={listId} todoId={todo.Id} />
                   </TooltipTrigger>
                   <TooltipContent>
